3d7d7662823b8d697407fe9a80e89f77b707b44d
Alfredo Colon <alfredoDevelops@gmail.com>
2020-08-29 15:27:26 -0700

    Initial Commit

---
+++ Objective-C Fundamentals I/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C Fundamentals I
+//
+//  Created by Alfredo Colon on 8/29/20.
+//  Copyright © 2020 Alfredo Colon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
